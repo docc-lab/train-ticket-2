6e1c5c317b9efdb7b2130db38e410df74012cfb9
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-11-25 14:32:50 -0500

    Add imports for fan-out bursty load generation

--- ts-basic-service/src/main/java/fdse/microservice/service/BasicServiceImpl.java
+++ ts-basic-service/src/main/java/fdse/microservice/service/BasicServiceImpl.java
@@ -17,6 +17,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import javax.annotation.PreDestroy;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author fdse
