634f34a6bb1a1049c44810b372abc71dd2a9eff6
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-11-28 21:32:18 -0500

    Fix import in consignpriceservice

--- ts-consign-price-service/src/main/java/consignprice/service/ConsignPriceServiceImpl.java
+++ ts-consign-price-service/src/main/java/consignprice/service/ConsignPriceServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author fdse
